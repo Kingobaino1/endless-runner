bfec161e73d7e9dd5959dc439bbb749c161b1357
Kingsley <kingobaino@gmail.com>
2021-03-20 17:48:57 +0100

    Include dependecies needed for the project and configure webpack

---
+++ dist/bundle.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+const messageEl = document.createElement('div');
+messageEl.textContent = 'I was put here by JavaScript!';
+document.body.appendChild(messageEl);
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9lbmRsZXNzLXJ1bm5lci8uL3NyYy9pbmRleC5qcyJdLCJuYW1lcyI6WyJtZXNzYWdlRWwiLCJkb2N1bWVudCIsImNyZWF0ZUVsZW1lbnQiLCJ0ZXh0Q29udGVudCIsImJvZHkiLCJhcHBlbmRDaGlsZCJdLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQSxNQUFNQSxTQUFTLEdBQUdDLFFBQVEsQ0FBQ0MsYUFBVCxDQUF1QixLQUF2QixDQUFsQjtBQUNBRixTQUFTLENBQUNHLFdBQVYsR0FBd0IsK0JBQXhCO0FBQ0FGLFFBQVEsQ0FBQ0csSUFBVCxDQUFjQyxXQUFkLENBQTBCTCxTQUExQixFIiwiZmlsZSI6ImJ1bmRsZS5qcyIsInNvdXJjZXNDb250ZW50IjpbImNvbnN0IG1lc3NhZ2VFbCA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoJ2RpdicpO1xubWVzc2FnZUVsLnRleHRDb250ZW50ID0gJ0kgd2FzIHB1dCBoZXJlIGJ5IEphdmFTY3JpcHQhJztcbmRvY3VtZW50LmJvZHkuYXBwZW5kQ2hpbGQobWVzc2FnZUVsKTsiXSwic291cmNlUm9vdCI6IiJ9
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpeg|jpg|svg|gif|mov|mp4)$/i,
+        use: {
+          loader: 'file-loader',
+          options: {
+            outputPath: 'media',
+            name: '[name].[ext]',
+          },
+        },
+      },
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
